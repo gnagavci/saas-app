7cf20e181141b0296700777bab31856b3a4aad4d
Gegë Nagavci <149297494+gnagavci@users.noreply.github.com>
2025-10-04 19:56:16 +0200

    add CompanionCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,45 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my Saas App</h1>
-      <Button>Let's get started</Button>
-    </div>
-  )
-}
+    <main>
+      <h1>Popular Companion</h1>
 
-export default Page
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+          />
+          <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives and Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+          />
+          <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="Language"
+          duration={30}
+          color="#bde7ff"
+          />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
