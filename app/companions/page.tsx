b6fa99f73be83666b7172068880832180262b5cf
Gegë Nagavci <149297494+gnagavci@users.noreply.github.com>
2025-10-10 20:49:21 +0200

    add SearchInput component implementation

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,30 +1,38 @@
 import CompanionCard from "@/components/CompanionCard";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
 import { getAllCompanions } from "@/lib/actions/companion.actions";
 import { getSubjectColor } from "@/lib/utils";
 
-const CompanionsLibrary = async ({searchParams}: SearchParams) => {
-  
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
   const filters = await searchParams;
   const subject = filters.subject ? filters.subject : "";
   const topic = filters.topic ? filters.topic : "";
 
-  const companions = await getAllCompanions({subject, topic});
+  const companions = await getAllCompanions({ subject, topic });
 
   return (
     <main>
       <section className="flex justify-between gap-4 max-sm:flex-col">
         <h1>Companion Library</h1>
-        <div className="flex gap-4">Filters</div>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
       </section>
       <section className="companions-grid">
         {companions.map((companion) => (
           // <CompanionCard key={companion.id} id={companion.id} name={companion.name} topic={companion.topic} subject={companion.subject} duration={companion.duration} color={getSubjectColor(companion.subject)}/>
           // smarter way to do this is to destructure the companion object so its properties can be automatically passed in
-          <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)}/>
-  ))}
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default CompanionsLibrary
+export default CompanionsLibrary;
